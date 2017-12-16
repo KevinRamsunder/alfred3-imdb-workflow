b263ecd489abc99fd03fce045f66e6b699008fe1
Kevin Ramsunder <kevinramsunder5@gmail.com>
2017-12-16 16:08:57 -0500

    switch to request, start tmp img work

---
+++ images/readme.md
@@ -0,0 +1,3 @@
+we need this directory to store the thumbnails
+
+it will be cleared periodically
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write alfred workflows in node
 const alfy = require( 'alfy' );
-const https = require( 'https' );
+const request = require( 'request' );
 
 // user input to alfred
 const input = alfy.input.replace( / /g, '_' );
@@ -10,34 +10,28 @@ const base  = 'https://v2.sg.media-imdb.com/suggests';
 const endpoint = `${ base }/${ input.charAt( 0 ) }/${ input }.json`;
 
 // make request
-https.get( endpoint, ( res ) => {
-  res.setEncoding( 'utf8' );
+request.get( { url: endpoint, json: false }, ( err, res, body ) => {
   let rawData = '';
 
-  // generate response text
-  res.on( 'data', ( chunk ) => { rawData = `${ rawData }${ chunk }` } );
+  // generate default response text
+  let items = [ { title: 'Nothing found!' } ];
 
-  // once request is finished
-  res.on( 'end', () => {
-    let items = [ { title: 'Nothing found!' } ];
+  // parse raw html into json object
+  const cleanedData = clean( body );
 
-    // parse raw html into json object
-    const cleanedData = clean( rawData );
+  // if we have a valid response, map it
+  if ( cleanedData && cleanedData.d ) {
+    items = cleanedData.d.map( ( d ) => {
+      return {
+        title: d.l,
+        subtitle: [ d.y, d.s ].filter( ( i ) => i ).join( ' - ' ),
+        arg: getURL( d.id ),
+      };
+    } );
+  }
 
-    // if we have a valid response, map it
-    if ( cleanedData && cleanedData.d ) {
-      items = cleanedData.d.map( ( d ) => {
-        return {
-          title: d.l,
-          subtitle: [ d.y, d.s ].filter( ( i ) => i ).join( ' - ' ),
-          arg: getURL( d.id ),
-        };
-      } );
-    }
-
-    // resolve to alfy so we can display
-    alfy.output( items );
-  } );
+  // resolve to alfy so we can display
+  alfy.output( items );
 } );
 
 // clean json we get back from IMDB, because its not actually json...
